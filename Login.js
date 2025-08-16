66280b8aeb2c8496fe49bf485f6980c99573bedf
Dokiu <dokiubr@gmail.com>
2025-08-16 18:32:19 -0300

    upload os arquivos
    icon.png (logo do site)
    index.html
    login.css
    login.js

---
+++ Login.js
@@ -0,0 +1,4 @@
+            function segredo(){
+                document.getElementById("T1").textContent = "Miau!"
+                document.getElementById("T2").textContent = "Não toca no Gato!!"
+            }
